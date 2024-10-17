c24552ceab48e425b2f8892ee15fd7fe41142197
Elvis <elvismrema9@gmail.com>
2024-10-17 17:05:50 +0300

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <x-slot:heading>About page</x-slot:heading>
+    <h1>Hello from the about 💻 page.</h1>
+</x-layout>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <x-slot:heading>Home page</x-slot:heading>
+    <h1>Hello from the home 🏡 page.</h1>
+</x-layout>
